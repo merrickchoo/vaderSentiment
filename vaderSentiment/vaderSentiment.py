c857c6a06ccc7c6455839925c1e2accbc9e78a1f
Francis Bond <bond@ieee.org>
2021-08-25 16:53:50 +0800

    changes to allow loading from a model

--- vaderSentiment/vaderSentiment.py
+++ vaderSentiment/vaderSentiment.py
@@ -30,42 +30,10 @@ B_DECR = -0.293
 C_INCR = 0.733
 N_SCALAR = -0.74
 
-NEGATE = \
-    ["aint", "arent", "cannot", "cant", "couldnt", "darent", "didnt", "doesnt",
-     "ain't", "aren't", "can't", "couldn't", "daren't", "didn't", "doesn't",
-     "dont", "hadnt", "hasnt", "havent", "isnt", "mightnt", "mustnt", "neither",
-     "don't", "hadn't", "hasn't", "haven't", "isn't", "mightn't", "mustn't",
-     "neednt", "needn't", "never", "none", "nope", "nor", "not", "nothing", "nowhere",
-     "oughtnt", "shant", "shouldnt", "uhuh", "wasnt", "werent",
-     "oughtn't", "shan't", "shouldn't", "uh-uh", "wasn't", "weren't",
-     "without", "wont", "wouldnt", "won't", "wouldn't", "rarely", "seldom", "despite"]
 
 # booster/dampener 'intensifiers' or 'degree adverbs'
 # http://en.wiktionary.org/wiki/Category:English_degree_adverbs
 
-BOOSTER_DICT = \
-    {"absolutely": B_INCR, "amazingly": B_INCR, "awfully": B_INCR, 
-     "completely": B_INCR, "considerable": B_INCR, "considerably": B_INCR,
-     "decidedly": B_INCR, "deeply": B_INCR, "effing": B_INCR, "enormous": B_INCR, "enormously": B_INCR,
-     "entirely": B_INCR, "especially": B_INCR, "exceptional": B_INCR, "exceptionally": B_INCR, 
-     "extreme": B_INCR, "extremely": B_INCR,
-     "fabulously": B_INCR, "flipping": B_INCR, "flippin": B_INCR, "frackin": B_INCR, "fracking": B_INCR,
-     "fricking": B_INCR, "frickin": B_INCR, "frigging": B_INCR, "friggin": B_INCR, "fully": B_INCR, 
-     "fuckin": B_INCR, "fucking": B_INCR, "fuggin": B_INCR, "fugging": B_INCR,
-     "greatly": B_INCR, "hella": B_INCR, "highly": B_INCR, "hugely": B_INCR, 
-     "incredible": B_INCR, "incredibly": B_INCR, "intensely": B_INCR, 
-     "major": B_INCR, "majorly": B_INCR, "more": B_INCR, "most": B_INCR, "particularly": B_INCR,
-     "purely": B_INCR, "quite": B_INCR, "really": B_INCR, "remarkably": B_INCR,
-     "so": B_INCR, "substantially": B_INCR,
-     "thoroughly": B_INCR, "total": B_INCR, "totally": B_INCR, "tremendous": B_INCR, "tremendously": B_INCR,
-     "uber": B_INCR, "unbelievably": B_INCR, "unusually": B_INCR, "utter": B_INCR, "utterly": B_INCR,
-     "very": B_INCR,
-     "almost": B_DECR, "barely": B_DECR, "hardly": B_DECR, "just enough": B_DECR,
-     "kind of": B_DECR, "kinda": B_DECR, "kindof": B_DECR, "kind-of": B_DECR,
-     "less": B_DECR, "little": B_DECR, "marginal": B_DECR, "marginally": B_DECR,
-     "occasional": B_DECR, "occasionally": B_DECR, "partly": B_DECR,
-     "scarce": B_DECR, "scarcely": B_DECR, "slight": B_DECR, "slightly": B_DECR, "somewhat": B_DECR,
-     "sort of": B_DECR, "sorta": B_DECR, "sortof": B_DECR, "sort-of": B_DECR}
 
 # check for sentiment laden idioms that do not contain lexicon words (future work, not yet implemented)
 SENTIMENT_LADEN_IDIOMS = {"cut the mustard": 2, "hand to mouth": -2,
@@ -82,17 +50,16 @@ SPECIAL_CASES = {"the shit": 3, "the bomb": 3, "bad ass": 1.5, "badass": 1.5, "b
 
 # #Static methods# #
 
-def negated(input_words, include_nt=True):
+def negated(input_words,
+            negators,
+            include_nt=True):
     """
     Determine if input contains negation words
     """
     input_words = [str(w).lower() for w in input_words]
-    neg_words = []
-    neg_words.extend(NEGATE)
-    for word in neg_words:
-        if word in input_words:
-            return True
-    if include_nt:
+    if any(w in negators for w in input_words):
+        return True
+    elif include_nt:
         for word in input_words:
             if "n't" in word:
                 return True
@@ -133,25 +100,6 @@ def allcap_differential(words):
         is_different = True
     return is_different
 
-
-def scalar_inc_dec(word, valence, is_cap_diff):
-    """
-    Check if the preceding words increase, decrease, or negate/nullify the
-    valence
-    """
-    scalar = 0.0
-    word_lower = word.lower()
-    if word_lower in BOOSTER_DICT:
-        scalar = BOOSTER_DICT[word_lower]
-        if valence < 0:
-            scalar *= -1
-        # check if booster/dampener word is in ALLCAPS (while others aren't)
-        if word.isupper() and is_cap_diff:
-            if valence > 0:
-                scalar += C_INCR
-            else:
-                scalar -= C_INCR
-    return scalar
 
 
 class SentiText(object):
@@ -194,12 +142,31 @@ class SentiText(object):
 class SentimentIntensityAnalyzer(object):
     """
     Give a sentiment intensity score to sentences.
-    """
 
+    The analyzer has a model (default "en_vader"), which loads 
+     * the lexicon
+     * emoji
+     * negative words
+     * intensifiers  FIXME
+     * diminishers   FIXME
+     * idioms        FIXME
+     * other constants
+
+    """
+    
+
+    
     def __init__(self,
                  model="en_vader",
                  lexicon_file="vader_lexicon.txt",
                  emoji_lexicon="emoji_utf8_lexicon.txt"):
+        """
+        First read all the lexicons from the model
+         * lexicon
+         * emoji
+         * negators
+         * boosters
+        """
         _this_module_file_path_ = os.path.abspath(getsourcefile(lambda: 0))
         lexicon_full_filepath = os.path.join(os.path.dirname(_this_module_file_path_),
                                              model,
@@ -215,6 +182,14 @@ class SentimentIntensityAnalyzer(object):
             self.emoji_full_filepath = f.read()
         self.emojis = self.make_emoji_dict()
 
+        ### changed to set
+        self.negators = self.read_words(source="negators.txt",
+                               model=model)
+        ### boosters
+        self.boosters = self.read_boosters(upfile="intensifiers.txt",
+                                           dnfile="diminishers.txt",
+                                           model=model)
+      
     def make_lex_dict(self):
         """
         Convert lexicon file to a dictionary
@@ -227,6 +202,37 @@ class SentimentIntensityAnalyzer(object):
             lex_dict[word] = float(measure)
         return lex_dict
 
+    def read_words(self, source="negators.txt", model="en_vader"):
+        """
+        Read a list of words from a file
+        Assume one word per line
+        """
+        words = set()
+        fh = os.path.join(os.path.dirname(os.path.abspath(getsourcefile(lambda: 0))),
+                          model,
+                          source)
+        for line in open(fh, encoding='utf-8'):
+            if line.startswith('#'):  ## allow comments
+                continue
+            w = line.strip()
+            if w:
+                words.add(w)
+        return words
+
+
+    def read_boosters(self,
+                     upfile="intensifiers.txt",
+                     dnfile="diminishers.txt",
+                     model="en_vader"):
+        boosters = dict()
+        uppers=self.read_words(upfile)
+        downers=self.read_words(upfile)
+        for w in uppers:
+            boosters[w] = B_INCR
+        for w in downers:
+            boosters[w] = B_DECR
+        return boosters
+            
     def make_emoji_dict(self):
         """
         Convert emoji lexicon file to a dictionary
@@ -237,6 +243,28 @@ class SentimentIntensityAnalyzer(object):
             emoji_dict[emoji] = description
         return emoji_dict
 
+
+    def scalar_inc_dec(self, word, valence, is_cap_diff):
+        """
+        Check if the preceding words increase, decrease, or negate/nullify the
+        valence
+        """
+        scalar = 0.0
+        word_lower = word.lower()
+        if word_lower in self.boosters:
+            scalar = self.boosters[word_lower]
+            if valence < 0:
+                scalar *= -1
+                # check if booster/dampener word is in ALLCAPS (while others aren't)
+                if word.isupper() and is_cap_diff:
+                    if valence > 0:
+                        scalar += C_INCR
+                    else:
+                        scalar -= C_INCR
+        return scalar
+
+
+    
     def polarity_scores(self, text):
         """
         Return a float for sentiment strength based on the input text.
@@ -266,7 +294,7 @@ class SentimentIntensityAnalyzer(object):
         for i, item in enumerate(words_and_emoticons):
             valence = 0
             # check for vader_lexicon words that may be used as modifiers or negations
-            if item.lower() in BOOSTER_DICT:
+            if item.lower() in self.boosters:
                 sentiments.append(valence)
                 continue
             if (i < len(words_and_emoticons) - 1 and item.lower() == "kind" and
@@ -311,7 +339,7 @@ class SentimentIntensityAnalyzer(object):
                 # (excluding the ones that immediately preceed the item) based
                 # on their distance from the current item.
                 if i > start_i and words_and_emoticons[i - (start_i + 1)].lower() not in self.lexicon:
-                    s = scalar_inc_dec(words_and_emoticons[i - (start_i + 1)], valence, is_cap_diff)
+                    s = self.scalar_inc_dec(words_and_emoticons[i - (start_i + 1)], valence, is_cap_diff)
                     if start_i == 1 and s != 0:
                         s = s * 0.95
                     if start_i == 2 and s != 0:
@@ -352,8 +380,8 @@ class SentimentIntensityAnalyzer(object):
                     sentiments.insert(si, sentiment * 1.5)
         return sentiments
 
-    @staticmethod
-    def _special_idioms_check(valence, words_and_emoticons, i):
+    #@staticmethod boosters depend on initialization
+    def _special_idioms_check(self, valence, words_and_emoticons, i):
         words_and_emoticons_lower = [str(w).lower() for w in words_and_emoticons]
         onezero = "{0} {1}".format(words_and_emoticons_lower[i - 1], words_and_emoticons_lower[i])
 
@@ -387,8 +415,8 @@ class SentimentIntensityAnalyzer(object):
         # check for booster/dampener bi-grams such as 'sort of' or 'kind of'
         n_grams = [threetwoone, threetwo, twoone]
         for n_gram in n_grams:
-            if n_gram in BOOSTER_DICT:
-                valence = valence + BOOSTER_DICT[n_gram]
+            if n_gram in self.boosters:
+                valence = valence + self.boosters[n_gram]
         return valence
 
     @staticmethod
@@ -405,11 +433,13 @@ class SentimentIntensityAnalyzer(object):
             valence = sum(idioms_valences) / float(len(idioms_valences))
         return valence
 
-    @staticmethod
-    def _negation_check(valence, words_and_emoticons, start_i, i):
+    # @staticmethod  ### negative words depend on the class
+    def _negation_check(self, valence, words_and_emoticons, start_i, i):
         words_and_emoticons_lower = [str(w).lower() for w in words_and_emoticons]
         if start_i == 0:
-            if negated([words_and_emoticons_lower[i - (start_i + 1)]]):  # 1 word preceding lexicon word (w/o stopwords)
+            # 1 word preceding lexicon word (w/o stopwords)
+            if negated([words_and_emoticons_lower[i - (start_i + 1)]],
+                       self.negators):
                 valence = valence * N_SCALAR
         if start_i == 1:
             if words_and_emoticons_lower[i - 2] == "never" and \
@@ -419,7 +449,10 @@ class SentimentIntensityAnalyzer(object):
             elif words_and_emoticons_lower[i - 2] == "without" and \
                     words_and_emoticons_lower[i - 1] == "doubt":
                 valence = valence
-            elif negated([words_and_emoticons_lower[i - (start_i + 1)]]):  # 2 words preceding the lexicon word position
+                
+            elif negated([words_and_emoticons_lower[i - (start_i + 1)]],
+                         self.negators):
+                # 2 words preceding the lexicon word position
                 valence = valence * N_SCALAR
         if start_i == 2:
             if words_and_emoticons_lower[i - 3] == "never" and \
@@ -429,7 +462,9 @@ class SentimentIntensityAnalyzer(object):
             elif words_and_emoticons_lower[i - 3] == "without" and \
                     (words_and_emoticons_lower[i - 2] == "doubt" or words_and_emoticons_lower[i - 1] == "doubt"):
                 valence = valence
-            elif negated([words_and_emoticons_lower[i - (start_i + 1)]]):  # 3 words preceding the lexicon word position
+            elif negated([words_and_emoticons_lower[i - (start_i + 1)]],
+                         self.negators):
+                # 3 words preceding the lexicon word position
                 valence = valence * N_SCALAR
         return valence
 
