45a1e998cdbda2124c63eb988748b7f71c334631
Francis Bond <bond@ieee.org>
2021-09-01 17:44:23 +0800

    moved eval.py and made path os indepedenent

--- eval.py
+++ eval.py
@@ -1,20 +1,23 @@
 import nltk
-import fileinput
-import string
-import re
+
+import os
 import vaderSentiment
-from vaderSentiment import SentimentIntensityAnalyzer
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from scipy import stats
 
 
-base_dict = []
-base_file = open(r'D:\VSC\vaderSentiment\additional_resources\hutto_ICWSM_2014\nytEditorialSnippets_GroundTruth.txt').readlines()
+
+
+gold_dir  = 'additional_resources/hutto_ICWSM_2014/'
+gold_file = 'nytEditorialSnippets_GroundTruth.txt'
+gold_fh   = open(os.path.join(gold_dir, gold_file)).readlines()
+
 analyzer = SentimentIntensityAnalyzer()
-for l in base_file:
+for l in gold_fh:
    id, score, sentence = l.strip().split('\t')
    e_score = analyzer.polarity_scores(sentence)['compound']
-   print(stats.spearmanr(score, e_score, axis=0, nan_policy='propagate', alternative='two-sided'))
-   print(id,score,sentence,e_score)
+#   print(stats.spearmanr(score, e_score, axis=0, nan_policy='propagate', alternative='two-sided'))
+   print(id,e_score,score,sentence)
 
 
 
