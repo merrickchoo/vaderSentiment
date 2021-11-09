9a71d8acb9593275ffcb1e2b9379ffc79d45009e
merrickchoo <merrick.emrys@gmail.com>
2021-11-09 18:19:47 +0800

    optimum combination of flags 11/9

--- eval_mono.py
+++ eval_mono.py
@@ -35,12 +35,17 @@ def eval_mono(list, flag):
         for s in wn.synsets(str):
             defn = [s.definition()]
             words = []
+
             if 'sto' in flag:
                 for sh in s.similar_tos():
                     words += sh.lemma_names()
             if 'sto_def' in flag:
                 for sh in s.similar_tos():
                     defn.append(sh.definition())
+            if 'sto_ex' in flag:      #hurts the accuracy
+                for sh in s.similar_tos():
+                    words += sh.examples()
+
             if 'hypo' in flag:
                 for sh in s.hyponyms():
                     words += sh.lemma_names()
@@ -50,21 +55,44 @@ def eval_mono(list, flag):
             if 'hypo_ex' in flag:      #hurts the accuracy
                 for sh in s.hyponyms():
                     words += sh.examples()
+
             if 'hype' in flag:
                 for sh in s.hypernyms():
                     words += sh.lemma_names()
+            if 'hype_def' in flag:      #hurts the accuracy
+                for sh in s.hypernyms():
+                    defn.append(sh.definition()) 
+            if 'hype_ex' in flag:      #hurts the accuracy
+                for sh in s.hypernyms():
+                    words += sh.examples()    
+    
             if 'i_hypo' in flag:        #not as important
                 for sh in s.instance_hyponyms():
                     words += sh.lemma_names()
             if 'i_hype' in flag:        #not as important
                 for sh in s.instance_hypernyms():
                     words += sh.lemma_names()
+
             if 'also' in flag:
                for sh in s.also_sees():
                   words += sh.lemma_names()
+            if 'also_def' in flag:      #hurts the accuracy
+                for sh in s.also_sees():
+                    defn.append(sh.definition())
+            if 'also_ex' in flag:       #hurts the accuracy      
+                for sh in s.also_sees():
+                    words += sh.examples()
+
             if 'att' in flag:
                for sh in s.attributes():
-                  words += sh.lemma_names()  
+                  words += sh.lemma_names() 
+            if 'att_def' in flag:       #hurts the accuracy
+                for sh in s.attributes():
+                    defn.append(sh.definition())
+            if 'att_ex' in flag:        #hurts the accuracy      
+                for sh in s.attributes():
+                    words += sh.examples()
+
             if 'ex' in flag:
                 words += s.examples()
             xdfn = '; '.join(defn+words)
@@ -91,7 +119,7 @@ def eval_mono(list, flag):
    #print(no_score)
    #print(prob_score)
 
-for flag in [[],['sto','sto_def','hypo','hypo_def','hype','also','att','ex','hypo_ex']]:
+for flag in [[],['sto', 'sto_def', 'hypo', 'hypo_def', 'hype', 'also', 'att', 'ex']]:
     print(flag)
     eval_mono(mono_list, flag)
 
