6498729086d01c56456d470b1e6c1401400527da
merrickchoo <merrick.emrys@gmail.com>
2022-01-27 09:41:54 +0800

    updatigng eval tests

--- eval.py
+++ eval.py
@@ -1,14 +1,17 @@
 import nltk
+from nltk.corpus import wordnet as wn
+from nltk.corpus.reader import wordnet
 import fileinput
 #import string
 #import re
+
 import vaderSentiment
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from scipy import stats
 
 #os.path
 
-def eval(evalfile):
+def eval_senti(evalfile):
    base_data = []
    score_list = []
    e_score_list = []
@@ -25,20 +28,52 @@ def eval(evalfile):
    print(evalfile, stats.pearsonr(score_list, e_score_list), stats.spearmanr(score_list, e_score_list, axis=0, nan_policy='propagate'))	
 
 
-eval('6humanCodedDataSets\SS_1041MySpace.txt')
-#eval('6humanCodedDataSets\SS_bbc1000.txt')
-#eval('6humanCodedDataSets\SS_digg1084.txt')
-#eval('6humanCodedDataSets\SS_rw1046.txt')
-eval('6humanCodedDataSets\SS_twitter4242.txt')
-eval('6humanCodedDataSets\YouTube3407.txt')
+#eval_senti('6humanCodedDataSets\SS_1041MySpace.txt')
+#eval_senti('6humanCodedDataSets\SS_twitter4242.txt')
+#eval_senti('6humanCodedDataSets\YouTube3407.txt')
 
-
+def eval_vader(evalfile):
+   base_data = []
+   score_list = []
+   e_score_list = []
+   d_score_list = []
+   base_file = open(evalfile).readlines()
+   analyzer = SentimentIntensityAnalyzer()
+   for l in base_file:
+      index, score, sentence = l.strip().split('\t')
+      for w in sentence:
+        for s in wn.synsets(w):
+            defn = [s.definition()]
+            words = []
+            for sh in s.similar_tos():
+               words += sh.lemma_names()
+            for sh in s.similar_tos():
+               defn.append(sh.definition())
+            for sh in s.hyponyms():
+               words += sh.lemma_names()
+            for sh in s.hyponyms():
+               defn.append(sh.definition())
+            for sh in s.hypernyms():
+               words += sh.lemma_names()
+            for sh in s.also_sees():
+               words += sh.lemma_names()
+            for sh in s.attributes():
+               words += sh.lemma_names() 
+            words += s.examples()
+            xdfn = '; '.join(defn+words)
+      d_score = analyzer.polarity_scores(xdfn)['compound']
+      e_score = analyzer.polarity_scores(sentence)['compound']
+      score_list.append(float(score))
+      e_score_list.append(e_score)
+      d_score_list.append(float(d_score))
+      base_data.append((score, e_score, sentence))
+   #print(base_data) #output sentiment scores
+   print(evalfile, stats.pearsonr(score_list, e_score_list), stats.spearmanr(score_list, e_score_list, axis=0, nan_policy='propagate'))	
+   print(evalfile, stats.pearsonr(d_score_list, e_score_list), stats.spearmanr(d_score_list, e_score_list, axis=0, nan_policy='propagate'))	
    
-#eval('additional_resources/hutto_ICWSM_2014/nytEditorialSnippets_GroundTruth.txt') 
-#eval('additional_resources/hutto_ICWSM_2014/amazonReviewSnippets_GroundTruth.txt')
-#eval('additional_resources/hutto_ICWSM_2014/movieReviewSnippets_GroundTruth.txt')
-#eval('additional_resources/hutto_ICWSM_2014/tweets_GroundTruth.txt')
+eval_vader('vaderSentiment\_additional_resources\hutto_ICWSM_2014\_amazonReviewSnippets_GroundTruth.txt') 
+eval_vader('vaderSentiment\_additional_resources\hutto_ICWSM_2014\movieReviewSnippets_GroundTruth.txt')
+eval_vader('vaderSentiment\_additional_resources\hutto_ICWSM_2014\_nytEditorialSnippets_GroundTruth.txt')
+eval_vader('vaderSentiment\_additional_resources\hutto_ICWSM_2014\_tweets_GroundTruth.txt')
 
-#def eval(model,filename):
-#    model=
-#    base = open(r'filename').read().split()
+
